f01a68483ac63485e3ebd5d13368936aff781efb
Rameez10 <Rameezmajid10@outlook.com>
2022-02-21 18:53:39 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+sudo pacman -Syu jdk-openjdk
+sudo pacman -S Wget
+wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/207/downloads/paper-1.18.1-207.jar
+wget https://dev.bukkit.org/projects/dynmap/files/3637280/download
